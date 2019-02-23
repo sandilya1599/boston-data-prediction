efe9cf19e2061416a3d8d131462aad194cc6778b
sandilya1599 <sandilya1599@gmail.com>
2019-02-23 12:32:44 +0530

    Added the dataset and changed the code

--- BostonPrediction.py
+++ BostonPrediction.py
@@ -9,12 +9,44 @@ Created on Thu Jan  3 22:36:40 2019
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
-dataset=pd.read_csv("train.csv")
-x_train,y_train=dataset.iloc[:,1:-1],dataset.iloc[:,-1]
-test_set=pd.read_csv("test.csv")
-x_test=test_set.iloc[:,1:]
-y_test=pd.read_csv("submission.csv")
-dec=LinearRegression().fit(x_train,y_train)
-y_test['medv']=dec.predict(x_test)
-y_test.to_csv("solution.csv",encoding="utf-8",index=False)
-#print(len(x_test),len(y_test))
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def dropColumns(xData,dataset):
+	# plot the data
+	sns.pairplot(dataset,x_vars=xData.columns.values,y_vars=['medv'])
+	plt.show()
+	# drop the columns that has no relation with the dependent variable
+	xData=xData.drop(columns=['chas','rad','zn','age','tax','b','indus','nox','crim','dis','ptratio'])
+	sns.pairplot(dataset,x_vars=xData.columns.values,y_vars=['medv'])
+	plt.show()
+	return xData
+def dataAnalysis(xData,dataset):
+	sns.set(rc={'figure.figsize':(11.7,8.27)})
+	sns.distplot(dataset['medv'], bins=30)
+	plt.show()
+	#find the correalted variables
+	corelationMatrix=dataset.corr().round(2)
+	sns.heatmap(data=corelationMatrix,annot=True)
+	plt.show()
+	xData=dropColumns(xData,dataset)
+	return xData
+def main():
+	#read the dataset
+	dataset=pd.read_csv("boston.csv")
+	# find dependent and independent variables
+	x=dataset.iloc[:,:-1]
+	y=dataset.iloc[:,-1]
+	x=dataAnalysis(x,dataset)
+	x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=45,train_size=0.7)
+	lr=LinearRegression()
+	#fit the train data
+	lr.fit(x_train,y_train)
+	#predict for the test data
+	y_pred=lr.predict(x_test)
+	error=mean_squared_error(y_test,y_pred)
+	print("The Mean squared error is {:.4f}".format(error))
+if __name__=='__main__':
+	main()
---
+++ README.md
@@ -0,0 +1,13 @@
+# Boston Housing Data Prediction
+
+Boston dataset contains the information collected by U.S.Census Service concerning in the area of Boston mass. The dataset is downloaded from [selva86](https://github.com/selva86/datasets/blob/master/BostonHousing.csv). 
+
+The data was originally published by Harrison, D. and Rubinfeld, D.L. `Hedonic prices and the demand for clean air', J. Environ. Economics & Management, vol.5, 81-102, 1978.`
+
+## Working
+
+Preprocess the data to remove unrelated variables from the data and then apply the linear regression to find the prices.
+
+## References
+
+[Medium](https://medium.com/@haydar_ai/learning-data-science-day-9-linear-regression-on-boston-housing-dataset-cd62a80775ef)
